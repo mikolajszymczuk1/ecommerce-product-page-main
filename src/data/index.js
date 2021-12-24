23682ff6dccf52d8a72a1fd623e419264e1e2435
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-12-24 15:25:45 +0100

    Create simple product data and store setup

---
+++ src/data/index.js
@@ -0,0 +1,30 @@
+// Shoes products
+const products = [
+    {
+        id: 0,
+        productImages: [
+            {
+                image: "../assets/images/image-product-1.jpg",
+                thumbnail: "../assets/images-product-1-thumbnail.jpg"
+            },
+            {
+                image: "../assets/images/image-product-2.jpg",
+                thumbnail: "../assets/images-product-2-thumbnail.jpg"
+            },
+            {
+                image: "../assets/images/image-product-3.jpg",
+                thumbnail: "../assets/images-product-3-thumbnail.jpg"
+            },
+            {
+                image: "../assets/images/image-product-4.jpg",
+                thumbnail: "../assets/images-product-4-thumbnail.jpg"
+            }
+        ],
+        name: "Fall Limited Edition Sneakers",
+        description: "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
+        price: 125,
+        discount: 50
+    }
+];
+
+export default products;
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,37 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import products from "../data";
 
 Vue.use(Vuex)
 
+const state = {
+    products: []
+}
+
+export const getters = {
+    // Return array of products
+    getAllProducts: (state) => state.products,
+    
+    // Return product with product id === id
+    getProductWithId: (state) => (id) => state.products[id]
+}
+
+export const mutations = {
+    // This mutation load data to products state
+    loadData(state) {
+        state.products = products;
+    }
+}
+
+export const actions = {
+    loadData(context) {
+        context.commit("loadData");
+    }
+}
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state,
+    getters,
+    mutations,
+    actions 
+});
