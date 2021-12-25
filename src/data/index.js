d330ed85b87c6cd7b45bd55a2e9abc08ef4c8f17
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-12-25 01:39:29 +0100

    Fix images paths

--- src/data/index.js
+++ src/data/index.js
@@ -4,25 +4,25 @@ const products = [
         id: 0,
         productImages: [
             {
-                image: "../assets/images/image-product-1.jpg",
-                thumbnail: "../assets/images-product-1-thumbnail.jpg"
+                image: "static/image-product-1.jpg",
+                thumbnail: "static/image-product-1-thumbnail.jpg"
             },
             {
-                image: "../assets/images/image-product-2.jpg",
-                thumbnail: "../assets/images-product-2-thumbnail.jpg"
+                image: "static/image-product-2.jpg",
+                thumbnail: "static/image-product-2-thumbnail.jpg"
             },
             {
-                image: "../assets/images/image-product-3.jpg",
-                thumbnail: "../assets/images-product-3-thumbnail.jpg"
+                image: "static/image-product-3.jpg",
+                thumbnail: "static/image-product-3-thumbnail.jpg"
             },
             {
-                image: "../assets/images/image-product-4.jpg",
-                thumbnail: "../assets/images-product-4-thumbnail.jpg"
+                image: "static/image-product-4.jpg",
+                thumbnail: "static/image-product-4-thumbnail.jpg"
             }
         ],
         name: "Fall Limited Edition Sneakers",
         description: "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
-        price: 125,
+        price: 250,
         discount: 50
     }
 ];
