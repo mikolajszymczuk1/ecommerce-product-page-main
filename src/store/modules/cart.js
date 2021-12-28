11f4f15329262d0e0ec94b567b13195533510625
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-12-28 14:06:12 +0100

    Create cart store module

---
+++ src/store/modules/cart.js
@@ -0,0 +1,50 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const state = {
+    items: []
+}
+
+export const getters = {
+    // Return true if cart is empty
+    isEmpty: (state) => state.items.length === 0,
+    
+    // Return price after discount
+    discountPrice: () => (product) => product.price - (product.price * (product.discount / 100)),
+
+    // Calculate total price of product
+    totalPriceOfItem: (state, getters) => (id) => {
+        let item = state.items.find((item) => item.product.id === id);
+        let discountPrice = getters.discountPrice(item.product);
+        return [...Array(item.quantity + 1).keys()].reduce((total) => total += discountPrice);
+    }
+}
+
+export const mutations = {
+    // Add item to cart
+    addItemToCart(state, product) {
+        let existItem = state.items.find((item) => item.product.id === product.id);
+        if (existItem) {
+            existItem.quantity++;
+        } else {
+            state.items.push({ product, quantity: 1 });
+        }
+    }
+}
+
+export const actions = {
+    // Add item to cart action
+    addItemToCart(context, product) {
+        context.commit("addItemToCart", product);
+    }
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions
+};
