288f60935b75abc0956b7fc0b72aa05a749de183
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-12-28 14:09:24 +0100

    Add click event to Add to cart button

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
 
                 <div class="product-details__prices-container">
                     <div class="product-details__prices-wrapper">
-                        <div class="product-details__discount-price">${{ discountPrice.toFixed(2) }}</div>
+                        <div class="product-details__discount-price">${{ discountPrice(product).toFixed(2) }}</div>
                         <div class="product-details__discount-value">{{ product.discount }}%</div>
                     </div>
 
@@ -30,7 +30,9 @@
                     <QuantityCounter />
 
                     <!-- Add to cart button -->
-                    <button class="product-details__add-to-cart-button">
+                    <button class="product-details__add-to-cart-button"
+                        @click="addItemToCart(product)"
+                    >
                         <svg class="product-details__cart-icon" viewBox="0 0 22 20" width="22" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#69707D" fill-rule="nonzero"/></svg>
                         Add to cart
                     </button>
@@ -57,6 +59,7 @@ export default {
     data() {
         return {
             product: {
+                id: 0,
                 title: "",
                 description: "",
                 price: 0,
@@ -67,15 +70,15 @@ export default {
     },
     computed: {
         ...mapGetters(["getProductWithId"]),
-        discountPrice() {
-            return this.product.price - (this.product.price * (this.product.discount / 100));
-        }
+        ...mapGetters("cart", ["discountPrice"]),
     },
     methods: {
-        ...mapActions([ "loadData" ]),
+        ...mapActions(["loadData"]),
+        ...mapActions("cart", ["addItemToCart"]),
 
         // Load update component product data
         updateComponentProductData(data) {
+            this.product.id = data.id;
             this.product.title = data.name;
             this.product.description = data.description;
             this.product.price = data.price;
