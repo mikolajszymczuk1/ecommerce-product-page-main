9e5b6d0a6d682d673af8511e74baa501b48ac7c3
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-12-29 02:12:35 +0100

    Catch emit from QuantityCounter component

--- src/App.vue
+++ src/App.vue
@@ -27,11 +27,11 @@
 
                 <!-- Quantity and Add to cart buttons -->
                 <div class="product-details__buttons-wrapper">
-                    <QuantityCounter />
+                    <QuantityCounter @counter-change="changeProductCount" />
 
                     <!-- Add to cart button -->
                     <button class="product-details__add-to-cart-button"
-                        @click="addItemToCart(product)"
+                        @click="addItemToCart({ product, count })"
                     >
                         <svg class="product-details__cart-icon" viewBox="0 0 22 20" width="22" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#69707D" fill-rule="nonzero"/></svg>
                         Add to cart
@@ -65,7 +65,8 @@ export default {
                 price: 0,
                 discount: 0,
                 images: []
-            }
+            },
+            count: 0
         }
     },
     computed: {
@@ -84,6 +85,11 @@ export default {
             this.product.price = data.price;
             this.product.discount = data.discount;
             this.product.images = data.productImages;
+        },
+
+        // Set new value of product count (quantity)
+        changeProductCount(e) {
+            this.count = e;
         }
     },
     created() {
